53bb63f90e8d90527daa53c373ed4532f9542860
Alwin Garside <alwin@garsi.de>
2025-10-01 12:17:47 +0200

    feat: `empaphy\usephul\Var\Type` now uses `ZEND_STR_*` constants and provides public constants itself in addition to enum cases.

--- src/Var/Type.php
+++ src/Var/Type.php
@@ -16,6 +16,16 @@ use ValueError;
 
 use function gettype;
 
+use const empaphy\usephul\ZEND_STR_ARRAY;
+use const empaphy\usephul\ZEND_STR_BOOLEAN;
+use const empaphy\usephul\ZEND_STR_CLOSED_RESOURCE;
+use const empaphy\usephul\ZEND_STR_DOUBLE;
+use const empaphy\usephul\ZEND_STR_INTEGER;
+use const empaphy\usephul\ZEND_STR_NULL;
+use const empaphy\usephul\ZEND_STR_OBJECT;
+use const empaphy\usephul\ZEND_STR_RESOURCE;
+use const empaphy\usephul\ZEND_STR_STRING;
+
 /**
  * Possible types as returned by {@see gettype()}.
  */
@@ -29,30 +39,30 @@ enum Type: string
      * Undefined and {@link https://www.php.net/unset unset()} variables will
      * also resolve to the value `null`.
      */
-    case Null = 'NULL';
+    public const NULL = ZEND_STR_NULL;
 
     /**
-     * The boolean type only has two values, and is used to express a truth
+     * The boolean type only has two values and is used to express a truth
      * value. It can be either `true` or `false`.
      */
-    case Boolean = 'boolean';
+    public const BOOLEAN = ZEND_STR_BOOLEAN;
 
     /**
      * An integer is a number of the set ℤ = {..., -2, -1, 0, 1, 2, ...}.
      */
-    case Integer = 'integer';
+    public const INTEGER = ZEND_STR_INTEGER;
 
     /**
      * A "float" is a floating point number, also known as a "double", or
      * "real number".
      */
-    case Float = 'double';
+    public const FLOAT = ZEND_STR_DOUBLE;
 
     /**
-     * A string is a series of characters, where a character is the same as
+     * A string is a series of characters where a character is the same as
      * a byte.
      */
-    case String = 'string';
+    public const STRING = ZEND_STR_STRING;
 
     /**
      * An array in PHP is actually an ordered map. A map is a type that
@@ -64,12 +74,12 @@ enum Type: string
      * {@link https://www.php.net/types.array array} values can be other
      * `array`s, trees and multidimensional `array`s are also possible.
      */
-    case Array = 'array';
+    public const ARRAY = ZEND_STR_ARRAY;
 
     /**
      * An object is an instance of a class or an enumeration.
      */
-    case Object = 'object';
+    public const OBJECT = ZEND_STR_OBJECT;
 
     /**
      * A resource is a special variable, holding a reference to an external
@@ -82,17 +92,88 @@ enum Type: string
      * See also the
      * {@link https://php.net/get_resource_type get_resource_type()} function.
      */
-    case Resource = 'resource';
+    public const RESOURCE = ZEND_STR_RESOURCE;
 
     /**
      * Closed resources are reported as "resource (closed)".
      */
-    case ClosedResource = 'resource (closed)';
+    public const CLOSED_RESOURCE = ZEND_STR_CLOSED_RESOURCE;
 
     /**
      * A value of an unknown type.
      */
-    case Unknown = 'unknown type';
+    public const UNKNOWN = 'unknown type';
+
+    /**
+     * The null type is PHP's unit type, i.e. it has only one value: NULL.
+     *
+     * Undefined and {@link https://www.php.net/unset unset()} variables will
+     * also resolve to the value `null`.
+     */
+    case Null = self::NULL;
+
+    /**
+     * The boolean type only has two values and is used to express a truth
+     * value. It can be either `true` or `false`.
+     */
+    case Boolean = self::BOOLEAN;
+
+    /**
+     * An integer is a number of the set ℤ = {..., -2, -1, 0, 1, 2, ...}.
+     */
+    case Integer = self::INTEGER;
+
+    /**
+     * A "float" is a floating point number, also known as a "double", or
+     * "real number".
+     */
+    case Float = self::FLOAT;
+
+    /**
+     * A string is a series of characters where a character is the same as
+     * a byte.
+     */
+    case String = self::STRING;
+
+    /**
+     * An array in PHP is actually an ordered map. A map is a type that
+     * associates values to keys.
+     *
+     * This type is optimized for several different uses; it can be treated
+     * as an array, list (vector), hash table (an implementation of a map),
+     * dictionary, collection, stack, queue, and probably more. As
+     * {@link https://www.php.net/types.array array} values can be other
+     * `array`s, trees and multidimensional `array`s are also possible.
+     */
+    case Array = self::ARRAY;
+
+    /**
+     * An object is an instance of a class or an enumeration.
+     */
+    case Object = self::OBJECT;
+
+    /**
+     * A resource is a special variable, holding a reference to an external
+     * resource. Resources are created and used by special functions.
+     *
+     * See the {@see https://www.php.net/resource appendix} for a listing of
+     * all these functions and the corresponding
+     * {@link https://www.php.net/types.resource resource} types.
+     *
+     * See also the
+     * {@link https://php.net/get_resource_type get_resource_type()} function.
+     */
+    case Resource = self::RESOURCE;
+
+    /**
+     * Closed resources are reported as "resource (closed)".
+     */
+    case ClosedResource = self::CLOSED_RESOURCE;
+
+    /**
+     * A value of an unknown type.
+     */
+    case Unknown = self::UNKNOWN;
 
     /**
      * Returns the Type instance of the provided value.
